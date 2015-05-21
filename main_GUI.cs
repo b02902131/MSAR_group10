d59c39fcc37c12c0a0e8ae77572e4c9808f81303
Bigyo <Bigyo@BigyodeMacBook-Pro.local>
2015-05-21 19:34:32 +0800

    add script

---
+++ main_GUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main_GUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "HELLO WORLD")) {
+			print ("press!");
+				}
+	}
+}
---
+++ recorder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class recorder : MonoBehaviour {
+	
+	bool isRecord = true;
+	float[] samples;
+
+	void Start(){
+
+	}
+
+	void OnGUI(){
+		if(isRecord){
+			if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-50,200,100), "Record")){ // 開始錄音
+				isRecord = !isRecord;
+				audio.clip = Microphone.Start("MyMicrophone", true, 1000, 44100); // 錄音1000秒，44100 Hz 格式
+			}
+		}else{
+			// 暫停錄音，並撥放錄音
+			print("sample = " + audio.clip.samples);	//44100 000 
+			if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-50,200,100), "Close & Play")){
+				isRecord = !isRecord;
+				Microphone.End("MyMicrophone");
+				audio.Play();
+			}
+		}
+		if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-150,200,100), "Get Data")){
+			samples = new float[audio.clip.samples * audio.clip.channels];
+			audio.clip.GetData(samples, 0);
+			int i = 0;
+			print ("sample freq = "+audio.clip.frequency);	//44100
+			print ("channel " + audio.clip.channels);
+			print("sample = " + audio.clip.samples);	//44100 000 
+			//print ("length = "+samples.Length);
+ 			while (i < 10){
+				samples[i] = samples[i] * 0.5F;
+				++i;
+				print(i + ": " + samples[i]);
+			}
+			audio.clip.SetData(samples, 0);
+		}
+
+	}
+	
+}
